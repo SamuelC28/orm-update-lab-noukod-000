731dae384044f9baad7edde2e9cfd8b393c266ee
Samuel Cornet <samuel@noukod.com>
2018-11-26 16:40:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,5 +38,14 @@ def save
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+ end
+
+ def self.new_from_db(row)
+  new_student = self.new
+  new_student.id = row[0]
+  new_student.name = row[1]
+  new_student.grade = row[2]
+  new_student
 end
+
 end
