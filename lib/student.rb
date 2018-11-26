4b496db94a1a5986d245bcdabf33c81dce8001fb
Samuel Cornet <samuel@noukod.com>
2018-11-26 16:32:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,9 +28,12 @@ class Student
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
+    if self.id 
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
